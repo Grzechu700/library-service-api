d90d29128ab75279852e89c6e23e31e498902708
Marcin <grzechnikmarcin@wp.pl>
2026-01-02 18:26:57 +0100

    Fix code style: use consistent double quotes in urls.py

--- library_service/urls.py
+++ library_service/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path('api/', include('books.urls')),
+    path("api/", include("books.urls")),
 ]
